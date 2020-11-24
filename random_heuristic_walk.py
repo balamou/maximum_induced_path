27fb0457fc39ec1ac926f49e4f11ecece8fb97c0
Michel Balamou <michelbalamou@gmail.com>
2020-11-24 18:34:46 -0500

    Able to find the induced path heuristically

--- random_heuristic_walk.py
+++ random_heuristic_walk.py
@@ -1,6 +1,7 @@
 from networkx.generators.random_graphs import erdos_renyi_graph
 import matplotlib.pyplot as plt
 import networkx as nx
+import random
 
 n = 6
 p = 0.5
@@ -22,5 +23,42 @@ def draw(edges):
     nx.draw_networkx_labels(G, pos)
     nx.draw_networkx_edges(G, pos, arrows=True)
     plt.show()
+
+def heuristic_solution(graph, initial_node = 0):
+    visited = {initial_node}
+    result = []
+    current_node = initial_node
+
+    while current_node != None:
+        adjacent_nodes = list(graph.adj[current_node])
+
+        result.append(current_node)
+
+        current_node = pick_random_node(adjacent_nodes, visited)
+        visited.update(adjacent_nodes)
+
+    return result
+
+
+def pick_random_node(array, visited):
+    index = pick_random_index(array)
+
+    while len(array) > 0 and array[index] in visited:
+        array.pop(index)
+        index = pick_random_index(array)
+
+    if len(array) == 0:
+        return None
+
+    return array[index]
+
+def pick_random_index(array):
+    if len(array) < 1:
+        return None
+    
+    index = random.randint(0, len(array) - 1)
+    return index
+
+print(heuristic_solution(g))
 
 draw(g.edges)
