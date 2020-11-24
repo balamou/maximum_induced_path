1663dd7d2f42097efe3c9cab209e5ae68365c586
Michel Balamou <michelbalamou@gmail.com>
2020-11-24 18:44:34 -0500

    Can show induced path on the graph

--- random_heuristic_walk.py
+++ random_heuristic_walk.py
@@ -3,28 +3,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import random
 
-n = 6
-p = 0.5
-g = erdos_renyi_graph(n, p)
-print(list(g.nodes))
-print(g.edges)
-print(type(g.edges))
-
-print(list(g.adj[1]))
-
-def draw(edges):
-    G = nx.DiGraph()
-    G.add_edges_from(edges)
-
-    # Need to create a layout when doing
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_size = 500)
-    # nx.draw_networkx_nodes(G, pos, nodelist=[first_node], cmap=plt.get_cmap('jet'), node_size = 500, node_color="red")
-    nx.draw_networkx_labels(G, pos)
-    nx.draw_networkx_edges(G, pos, arrows=True)
-    plt.show()
-
-def heuristic_solution(graph, initial_node = 0):
+def heuristic_induced_path(graph, initial_node = 0):
     visited = {initial_node}
     result = []
     current_node = initial_node
@@ -59,6 +38,31 @@ def pick_random_index(array):
     index = random.randint(0, len(array) - 1)
     return index
 
-print(heuristic_solution(g))
+def draw(edges, induced_path = []):
+    G = nx.DiGraph()
+    G.add_edges_from(edges)
 
-draw(g.edges)
+    # Need to create a layout when doing
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_size = 500)
+    nx.draw_networkx_nodes(G, pos, nodelist=induced_path, cmap=plt.get_cmap('jet'), node_size = 500, node_color="red")
+    nx.draw_networkx_labels(G, pos)
+    nx.draw_networkx_edges(G, pos, arrows=False)
+    plt.show()
+
+def main():
+    total_nodes = 6
+    p = 0.5 # probability of edge creation
+    graph = erdos_renyi_graph(total_nodes, p)
+    print(list(graph.nodes))
+    print(graph.edges)
+    print(type(graph.edges))
+
+    print(list(graph.adj[1]))
+
+    heuristic = heuristic_induced_path(graph)
+    print("Heuristic solution:")
+    print(heuristic)
+    draw(graph.edges, heuristic)
+
+main()
