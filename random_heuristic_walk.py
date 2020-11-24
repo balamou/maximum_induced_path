584b4b8abfb874ac8b7d2bde6a73d786ceb201ce
Michel Balamou <michelbalamou@gmail.com>
2020-11-24 17:08:17 -0500

    Added adjecency

--- random_heuristic_walk.py
+++ random_heuristic_walk.py
@@ -5,16 +5,17 @@ import networkx as nx
 n = 6
 p = 0.5
 g = erdos_renyi_graph(n, p)
-print(g.nodes)
+print(list(g.nodes))
 print(g.edges)
 print(type(g.edges))
+
+print(list(g.adj[1]))
 
 def draw(edges):
     G = nx.DiGraph()
     G.add_edges_from(edges)
 
     # Need to create a layout when doing
-    # first_node = edges[0][0]
     pos = nx.spring_layout(G)
     nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_size = 500)
     # nx.draw_networkx_nodes(G, pos, nodelist=[first_node], cmap=plt.get_cmap('jet'), node_size = 500, node_color="red")
