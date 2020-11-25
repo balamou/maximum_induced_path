54c026e92156aaaf19942ea4c4cbfe10792fd141
Michel Balamou <michelbalamou@gmail.com>
2020-11-24 19:22:16 -0500

    Iterates several times to find the maximum induced path

--- random_heuristic_walk.py
+++ random_heuristic_walk.py
@@ -3,6 +3,22 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import random
 
+
+def several_rounds_induced(graph, iterations = 20): 
+    """
+    Keeps finding induced paths randomly and returns the one with the max length
+    """
+    max_induced_path = []
+    
+    for _ in range(iterations):
+        found_induced_path = heuristic_induced_path(graph)
+        print(f'Len: {len(found_induced_path)}, path: {found_induced_path}')
+        if len(found_induced_path) > len(max_induced_path):
+            max_induced_path = found_induced_path
+
+    return max_induced_path
+
+
 def heuristic_induced_path(graph):
     initial_node = pick_random_node(graph)
     visited = {initial_node}
@@ -54,7 +70,7 @@ def draw(edges, induced_path = []):
     G = nx.DiGraph()
     G.add_edges_from(edges)
 
-    pos = nx.planar_layout(G) # Layout of the position of nodes
+    pos = nx.circular_layout(G) # Layout of the position of nodes
     nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_size = 500)
     nx.draw_networkx_nodes(G, pos, nodelist=induced_path, cmap=plt.get_cmap('jet'), node_size = 500, node_color="red")
     nx.draw_networkx_labels(G, pos)
@@ -62,18 +78,18 @@ def draw(edges, induced_path = []):
     plt.show()
 
 def main():
-    total_nodes = 6
-    p = 0.5 # probability of edge creation
-    graph = erdos_renyi_graph(total_nodes, p, 2493)
+    total_nodes = 12 # total nodes created
+    p = 0.2 # probability of edge creation
+    graph = erdos_renyi_graph(total_nodes, p, 3245)
     print(list(graph.nodes))
     print(graph.edges)
     print(type(graph.edges))
 
     print(list(graph.adj[1]))
 
-    heuristic = heuristic_induced_path(graph)
+    heuristic = several_rounds_induced(graph)
     print("Heuristic solution:")
-    print(heuristic)
+    print(f'Len: {len(heuristic)}, {heuristic}')
     draw(graph.edges, heuristic)
 
 main()
